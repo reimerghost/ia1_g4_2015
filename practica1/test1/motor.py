fded25585d77dd5c8750752184226287d58eb02e
ReimerGhost <rchamale10@gmail.com>
2015-08-22 01:54:17 +0000

    12333

--- practica1/test1/motor.py
+++ practica1/test1/motor.py
@@ -1,4 +1,3 @@
-
 import RPi.GPIO as GPIO
 from time import sleep
 #Inicializando GPIO a modo BCM
@@ -10,15 +9,44 @@ GPIO.setup(18, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
 GPIO.setup(23, GPIO.OUT)
 
+### -- METODOS DE MOVIMIENTO -- ###
+def irAdelante():
+	GPIO.output(17, True)
+	GPIO.output(18, False)
+	GPIO.output(23, False)
+	GPIO.output(22, True)
 
-#while 1:
-GPIO.output(17, True)
-GPIO.output(18, True)
+def irAtras():
+        GPIO.output(17, False)
+        GPIO.output(18, True)
+        GPIO.output(23, True)
+        GPIO.output(22, False)
 
-GPIO.output(23, True)
-GPIO.output(22, True)
+def irIzquierda():
+        GPIO.output(17, False)
+        GPIO.output(18, False)
+        GPIO.output(23, False)
+        GPIO.output(22, True)
+
+def irDerecha():
+        GPIO.output(17, True)
+        GPIO.output(18, False)
+        GPIO.output(23, False)
+        GPIO.output(22, False)
+
+def parar():
+	GPIO.output(17, False)
+	GPIO.output(18, False)
+	GPIO.output(23, False)
+	GPIO.output(22, False)
 #18 ya
 #17 ya
 #22
 #23
 
+#def __init__(self):
+#irAdelante()
+#irAtras()
+#irDerecha()
+#irIzquierda()
+parar()
