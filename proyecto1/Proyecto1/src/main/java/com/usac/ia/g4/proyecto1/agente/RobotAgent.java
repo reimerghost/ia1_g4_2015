34c403df4da62c0d0d4cf5146789e7345e09141a
nyandranzz <dranzer.the.dark@gmail.com>
2015-09-25 23:28:32 -0600

    robot_agent
    Agregado primer agente "HAL9000"

---
+++ proyecto1/Proyecto1/src/main/java/com/usac/ia/g4/proyecto1/agente/RobotAgent.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.usac.ia.g4.proyecto1.agente;
+
+import jade.core.Agent;
+
+/**
+ *
+ * @author Fernando
+ */
+public class RobotAgent extends Agent{
+    
+    public void setup(){
+        System.out.println("Hola mundo, soy el agente "+getLocalName());
+    }
+}
