58f37a62b410441a9a3370c9e1d7c5d0206c836c
ReimerGhost <rchamale10@gmail.com>
2015-09-30 15:56:52 -0600

    Driver Carrito (iniciando)

---
+++ proyecto1/Proyecto1/src/main/java/com/usac/ia/g4/proyecto1/rasp/driverCarrito.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.usac.ia.g4.proyecto1.rasp;
+
+/**
+ *
+ * @author reimer
+ */
+public class driverCarrito {
+
+    public void Adelante() {
+        System.out.println("GO GO Adelante");
+    }
+
+    public void Atras() {
+        System.out.println("Atras");
+    }
+
+    public void giraIzquierda() {
+        System.out.println("90° a las Izquierda");
+    }
+
+    public void giraDerecha() {
+        System.out.println("90° a la Derecha");
+    }
+
+}
